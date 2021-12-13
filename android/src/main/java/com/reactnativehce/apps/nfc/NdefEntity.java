4da6371f2bb664414a686893cfddfc44cd076c24
mptr <54647612+mptr@users.noreply.github.com>
2021-12-13 22:52:59 +0100

    it just works

--- android/src/main/java/com/reactnativehce/apps/nfc/NdefEntity.java
+++ android/src/main/java/com/reactnativehce/apps/nfc/NdefEntity.java
@@ -12,11 +12,11 @@ public class NdefEntity {
   public static String TAG = "NdefEntity";
   NdefRecord record;
 
-  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
   public NdefEntity(String type, String content) {
 
     if (type.equals("text")) {
-      record = createTextRecord("en", content);
+      record = createTextRecord(content);
     } else if (type.equals("url")) {
       record = createUrlRecord(content);
     } else if (type.equals("json")) {
@@ -28,55 +28,23 @@ public class NdefEntity {
     }
   }
 
-  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-  public static NdefRecord createTextRecord(String language, String text) {
-    byte[] languageBytes;
-    byte[] textBytes;
-
-    languageBytes = language.getBytes(StandardCharsets.US_ASCII);
-    textBytes = text.getBytes(StandardCharsets.UTF_8);
-
-    byte[] recordPayload = new byte[1 + (languageBytes.length & 0x03F) + textBytes.length];
-
-    int zeroVal = languageBytes.length & 0x03F;
-    recordPayload[0] = (byte) zeroVal;
-
-    System.arraycopy(languageBytes, 0, recordPayload, 1, languageBytes.length & 0x03F);
-    System.arraycopy(textBytes, 0, recordPayload,1 + (languageBytes.length & 0x03F), textBytes.length);
-
-    Log.i(TAG, BinaryUtils.ByteArrayToHexString(recordPayload));
-
-    return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, null, recordPayload);
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  public static NdefRecord createTextRecord(String text) {
+    return NdefRecord.createTextRecord(null, text);
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   public static NdefRecord createUrlRecord(String text) {
-    byte[] textBytes;
-
-    textBytes = text.getBytes(StandardCharsets.UTF_8);
-
-    byte[] recordPayload = new byte[1 + textBytes.length];
-
-    System.arraycopy(textBytes,0, recordPayload,1, textBytes.length);
-
-    Log.i(TAG, BinaryUtils.ByteArrayToHexString(recordPayload));
-
-    return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_URI, null, recordPayload);
+    return NdefRecord.createUri(text);
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   public static NdefRecord createJsonRecord(String text) {
-    byte[] textBytes;
-
-    textBytes = text.getBytes(StandardCharsets.UTF_8);
-
-    byte[] recordPayload = new byte[1 + textBytes.length];
-
-    System.arraycopy(textBytes,0, recordPayload,1, textBytes.length);
-
-    Log.i(TAG, BinaryUtils.ByteArrayToHexString(recordPayload));
-
-    return NdefRecord.createMime("application/json", textBytes);
+    return NdefRecord.createMime(
+      "application/json",
+      language.getBytes(StandardCharsets.US_ASCII),
+      text.getBytes(StandardCharsets.UTF_8)
+    );
   }
 
   public static NdefRecord createAppRecord(String text) {
